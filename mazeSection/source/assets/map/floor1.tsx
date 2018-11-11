5561f3353f437e69ef40f1ba7e98f33f16c6f61e
IzzyRose <isabellegreenberg@gmail.com>
2018-11-11 10:21:28 -0500

    Adjusted file structure for team

---
+++ mazeSection/source/assets/map/floor1.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="floor1" tilewidth="100" tileheight="100" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="100" height="100" source="../../../../../../Downloads/New Piskel (4).png"/>
+ </tile>
+ <tile id="1">
+  <image width="100" height="100" source="../../../../../../Downloads/New Piskel (5).png"/>
+ </tile>
+ <tile id="2">
+  <image width="100" height="100" source="../../../../../../Downloads/New Piskel (7).png"/>
+ </tile>
+ <tile id="3">
+  <image width="100" height="100" source="../../../../../../Downloads/New Piskel (7).png"/>
+ </tile>
+ <tile id="4">
+  <image width="100" height="100" source="../../../../../../Downloads/New Piskel (7).png"/>
+ </tile>
+</tileset>
